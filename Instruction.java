81e67f57fbc914c3c5f24ac233510b8999e21651
NalanNagaradje <157830927+NalanNagaradje@users.noreply.github.com>
2024-02-07 13:34:26 +0100

    Add files via upload
    Test Instruction

---
+++ Instruction.java
@@ -0,0 +1,18 @@
+
+
+public class Instruction {
+
+    public static void main(String[] args){
+        String s1= "LINK 200";
+        String[] s2= s1.split("\\s");
+
+        for(String s: s2 ){
+
+            System.out.println(s);
+        }
+
+
+    }
+
+
+}
