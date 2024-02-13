3d33e004c6511b3c70102f54783dcb33e1244aaf
Anis <anis0910elabed@gmail.com>
2024-02-13 18:02:56 +0100

    new changes in GaphicInterface class

--- GraphicInterface.java
+++ GraphicInterface.java
@@ -1,2 +1,71 @@
-package PACKAGE_NAME;public class GraphicInterface {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GraphicInterface {
+    private static int currentLine = 0;
+    private static JTabbedPane robotTabs = new JTabbedPane();
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Exapunks Clone");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1200, 800);
+        frame.setLayout(new BorderLayout());
+
+        // Ajout des zones de texte pour différents robots
+        JTextArea codeArea1 = new JTextArea(20, 40);
+        robotTabs.addTab("Robot 1", new JScrollPane(codeArea1));
+        JTextArea codeArea2 = new JTextArea(20, 40);
+        robotTabs.addTab("Robot 2", new JScrollPane(codeArea2));
+        frame.add(robotTabs, BorderLayout.WEST);
+
+        // Zone de jeu
+        JPanel gamePanel = new JPanel();
+        // TODO: Ajoutez la logique de la zone de jeu
+        gamePanel.setPreferredSize(new Dimension(600, 600));
+        frame.add(gamePanel, BorderLayout.CENTER);
+
+        // Panneau pour les boutons
+        JPanel buttonPanel = new JPanel();
+        JButton stepButton = new JButton("Pas");
+        JButton stopButton = new JButton("Stop");
+
+        stepButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Obtenez la zone de texte actuelle
+                JTextArea currentCodeArea = (JTextArea) ((JScrollPane) robotTabs.getSelectedComponent()).getViewport().getView();
+                executeNextInstruction(currentCodeArea);
+            }
+        });
+
+        stopButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JTextArea currentCodeArea = (JTextArea) ((JScrollPane) robotTabs.getSelectedComponent()).getViewport().getView();
+                resetRobot(currentCodeArea);
+            }
+        });
+
+        buttonPanel.add(stepButton);
+        buttonPanel.add(stopButton);
+        frame.add(buttonPanel, BorderLayout.SOUTH);
+
+        frame.setVisible(true);
+    }
+
+    private static void executeNextInstruction(JTextArea codeArea) {
+        String[] lines = codeArea.getText().split("\n");
+        if (currentLine < lines.length) {
+            String instruction = lines[currentLine];
+            currentLine++;
+            // TODO: Ajoutez la logique pour exécuter l'instruction
+        }
+    }
+
+    private static void resetRobot(JTextArea codeArea) {
+        currentLine = 0;
+        // TODO: Ajoutez toute autre logique de réinitialisation nécessaire
+    }
 }
