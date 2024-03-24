1fd799e613e233c27742ebe4405005ddb1c56deb
NalanNagaradje <157830927+NalanNagaradje@users.noreply.github.com>
2024-03-24 23:11:23 +0100

    Add files via upload

---
+++ JeuFinal.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Jeu{
+
+        public static void main (String [] args){
+
+                Scanner scanner = new Scanner(System.in);
+                Scanner scan  = new Scanner(System.in); 
+                Scanner nextLine = new Scanner(System.in); //Scanne la touche entrer a chaque parcours de la liste d'instruction
+                Scanner Choix = new Scanner(System.in);
+
+                boolean quit=false;// Represente la condition de fermeture du jeux
+                String level;
+
+                while(!quit1){
+                        System.out.println();
+                        System.out.println("Choose game Level or quit with q or Q \n Level 1\n Level 2\n Level 3\n");
+                        level = scanner.nextLine();
+                        
+                        // choix du niveau //
+                        switch(level){
+                                
+                                case "1":
+                                        new Lvl1();   
+                                        break;
+
+                                case "2":
+                                        // new Lvl2();
+                                        break;
+                                
+                                case "3":
+                                        new Lvl3();
+                                        break;
+
+                                case "q": case "Q" :
+                                        
+                                        quit=true;
+                                        break;
+                                
+                                        default : System.out.println("Choose Level 1 to 3 !!!");
+
+                        }   
+                }
+                Choix.close();
+                scan.close();
+                nextLine.close();
+                scanner.close();
+        }
+}
